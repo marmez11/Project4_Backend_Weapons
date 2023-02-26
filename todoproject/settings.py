a0c0e1f082b5518a8aef890721e72a7df519f518
marmez11 <116234002+marmez11@users.noreply.github.com>
2023-02-26 14:40:03 -0800

    Add files via upload

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -85,16 +85,27 @@ WSGI_APPLICATION = "todoproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        conn_max_age=600,
+#        conn_health_checks=True,
+#    ),
+#}
+
+#DATABASES['default'] =  dj_database_url.config()
+
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-    ),
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+    'NAME': 'marmez11/rchrch',
+    'HOST': 'db.bit.io',
+    'PORT': '5432',                    
+    'USER': 'marmez11',
+    'PASSWORD': 'v2_3zZvT_e7txCS2bt254eLCZH7BQjcF',
+    }
 }
-
-DATABASES['default'] =  dj_database_url.config()
-
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
